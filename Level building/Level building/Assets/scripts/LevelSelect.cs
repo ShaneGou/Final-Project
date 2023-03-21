18dd1aaf27856d8c5cbb6bc66c9b09d035ce16b5
glickmas <glickmas@gmail.com>
2023-03-21 11:50:22 -0700

    adds intro scene to select level
    creates ugly version of start screen; changes build settings: adding all scenes; minor changes to hard level

---
+++ Level building/Level building/Assets/scripts/LevelSelect.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    [SerializeField] int levelNumber = 0;
+
+    public void OpenScene() {
+        SceneManager.LoadScene(levelNumber, LoadSceneMode.Single);
+    }
+}
