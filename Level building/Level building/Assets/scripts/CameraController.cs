c4d8ec3404e6c164ade3f84072ffeba2fab7de77
glickmas <glickmas@gmail.com>
2023-03-24 17:47:13 -0700

    does more cleanup
    adjusts level health, etc.

--- Level building/Level building/Assets/scripts/CameraController.cs
+++ Level building/Level building/Assets/scripts/CameraController.cs
@@ -6,49 +6,38 @@ public class CameraController : MonoBehaviour
 {
 
     public Transform target;
-
     public Vector3 offset;
-
     public bool useOffsetValues;
-
     public float rotateSpeed;
-
     public Transform pivot;
-
     public float maxViewAngle;
     public float minViewAngle;
-
     public bool invertY;
-    // Start is called before the first frame update
+
     void Start()
     {
         if (!useOffsetValues)
         {
-
             offset = target.position - transform.position;
-
         }
 
         pivot.transform.position = target.transform.position;
-        //pivot.transform.parent = target.transform;
         pivot.transform.parent = null;
        
         Cursor.lockState = CursorLockMode.Locked;// when get into the game can lhide the cursor
     }
-    // Update is called once per frame
+
     void LateUpdate()
     {
-
         pivot.transform.position = target.transform.position;
 
         // get the X position of the mouse & rotate the target
         float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
         pivot.Rotate(0, horizontal, 0);
 
-
         // Get the Y position of the mouse & rotate the pivot
         float vertical = Input.GetAxis("Mouse Y")* rotateSpeed;
-       // pivot.Rotate(-vertical, 0, 0);
+
        if(invertY)
         {
             pivot.Rotate(vertical, 0, 0);
@@ -59,12 +48,12 @@ public class CameraController : MonoBehaviour
         }
 
         // limit up/down camera rotation
-        if(pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+        if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
         {
             pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
         }
 
-        if(pivot.rotation.eulerAngles.x > 180 && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        if (pivot.rotation.eulerAngles.x > 180 && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
         {
             pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
         }
@@ -75,9 +64,7 @@ public class CameraController : MonoBehaviour
         Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
         transform.position = target.position - (rotation * offset);
 
-        //transform.position = target.position - offset;
-        
-        if(transform.position.y < target.position.y)
+        if (transform.position.y < target.position.y)
         {
             transform.position = new Vector3(transform.position.x, target.position.y -.5f, transform.position.z);
         }
